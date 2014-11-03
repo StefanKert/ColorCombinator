ea9ad28569f0cb9414df36761cf6447fc3241840
Stefan Kert <stefan.kert@inode.at>
2014-11-03 18:10:02 +0100

    Added angular with controller

---
+++ javascripts/app.js
@@ -0,0 +1,55 @@
+angular.module('ColorCombinatorApp', [])
+	.controller('ColorCombinatorController', ['$scope', function($scope) {
+		$scope.elements = 5;
+		$scope.combinationCounter = 0;
+		$scope.changeValues = function() {
+			var elements = prompt("Bitte die gewünschte Anzahl an Elementen eingeben (1-10)", "3");		
+			if (elements != null && elements > 0 && elements < 8) {
+				$scope.elements = elements;
+			}
+			else {
+				MessageBox.Show("Es wurde ein ungültiger Wert angegeben daher wird der Wert 5 verwendet.");
+				$scope.elements = 5;
+			}
+		};
+		
+		$scope.combineColors = function() {
+			var counter = 0;
+			var colors = ["#000000", "#0000ff", "#00ff00", "#00ffff", "#ff0000", "#ff00ff", "#ffff00", "#ffffff"];
+			var values = [];
+			var contentElement = $("#content");
+			
+			function printCombinations(n){
+				if(n > 0){
+					var shouldContinue = false;
+					for(var element = 0; element < $scope.elements; element++){
+						for(var i = 0; i < values.length; i++) {
+							if(values[i] === element){								
+								shouldContinue = true;
+								break;
+							}
+						}
+						if(shouldContinue){
+							shouldContinue = false;
+							continue;
+						}
+						values.push(element);
+						printCombinations(n - 1);
+						values.pop(element);
+					}
+				} 
+				else {
+					for(var i = 0; i < values.length; i++) {
+						contentElement.append("<span style='background: " + colors[values[i]] + "'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;&nbsp;&nbsp;");
+					}
+					contentElement.append("<br>");
+					$scope.combinationCounter++;
+				}
+			}
+			printCombinations($scope.elements);
+			alert("Es wurden insgesamt " + $scope.combinationCounter + " Elemente gezeichnet.");
+		};		
+		$scope.combineColors();
+	};
+}]);
+
