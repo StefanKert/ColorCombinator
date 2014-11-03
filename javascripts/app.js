5ca8f583b0df43291cc651e567dbe8c7b2e76f42
Stefan Kert <stefan.kert@inode.at>
2014-11-03 22:21:24 +0100

    Fixed a view issues
    Better performance and many other features

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,21 +1,35 @@
-angular.module('ColorCombinatorApp', [])
+angular.module('ColorCombinatorApp', ['ngSanitize'])
 	.controller('ColorCombinatorController', ['$scope', function($scope) {
+		$scope.showLoading = false;
 		$scope.elementsCount = 5;
 		$scope.combinationCounter = 0;
-		$scope.changeValues = function() {
-			var elements = prompt("Bitte die gewünschte Anzahl an Elementen eingeben (1-10)", "3");		
-			if (elements != null && elements > 0 && elements < 8) {
-				$scope.elements = elements;
-			}
-			else {
-				MessageBox.Show("Es wurde ein ungültiger Wert angegeben daher wird der Wert 5 verwendet.");
-				$scope.elements = 5;
-			}
-		};
+		$scope.elements = [];
 		
 		$scope.combineColors = function() {
+			$scope.elements = [];
+			var opts = {
+			  lines: 13, // The number of lines to draw
+			  length: 20, // The length of each line
+			  width: 10, // The line thickness
+			  radius: 30, // The radius of the inner circle
+			  corners: 1, // Corner roundness (0..1)
+			  rotate: 10, // The rotation offset
+			  direction: 1, // 1: clockwise, -1: counterclockwise
+			  color: '#000', // #rgb or #rrggbb or array of colors
+			  speed: 2.2, // Rounds per second
+			  trail: 60, // Afterglow percentage
+			  shadow: false, // Whether to render a shadow
+			  hwaccel: false, // Whether to use hardware acceleration
+			  className: 'spinner', // The CSS class to assign to the spinner
+			  zIndex: 2e9, // The z-index (defaults to 2000000000)
+			  top: '50%', // Top position relative to parent
+			  left: '50%' // Left position relative to parent
+			};
+			var target = document.getElementById('loading');
+			var spinner = new Spinner(opts).spin(target);
+			$scope.showLoading = true;
 			var counter = 0;
-			var colors = ["#000000", "#0000ff", "#00ff00", "#00ffff", "#ff0000", "#ff00ff", "#ffff00", "#ffffff"];
+			var colors = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth"];
 			var values = [];
 			var contentElement = $("#content");
 			
@@ -39,21 +53,35 @@ angular.module('ColorCombinatorApp', [])
 					}
 				} 
 				else {
+					var html = "";
 					for(var i = 0; i < values.length; i++) {
-						contentElement.append("<span style='background: " + colors[values[i]] + "'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;&nbsp;&nbsp;");
+						html += '<span class="' + colors[values[i]] + '">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;&nbsp;&nbsp;';
 					}
-					contentElement.append("<br>");
+					html += "<br>";
+					$scope.elements.push({context: html});
 					$scope.combinationCounter++;
 				}
 			}
 			
-			if($scope.elementsCount > 7){
-				toastr.error('Bitte geben Sie keine Werte &uuml;ber 7 ein.');	
+			if($scope.elementsCount > 10){
+				var calculatedCount = $scope.calculateFaculty($scope.elementsCount);
+				toastr.error('Sie haben den Wert ' + $scope.elementsCount + ' eingegeben. Mit diesem Wert w&uuml;rden sich ' + calculatedCount + ' Kombinationen ergeben und ich denke wir alle wissen, dass niemand so viel Zeit h&auml;tte dass anzusehen.');	
+				spinner.stop();
 				return;
 			}
 			
 			$scope.combinationCounter = 0;
-			contentElement.empty();
 			printCombinations($scope.elementsCount);
+			spinner.stop();
+			console.debug($scope.elements.length);
+			$scope.showLoading = false;
 		};		
+		$scope.calculateFaculty = function(n) {
+			if(n > 1){
+				return n * $scope.calculateFaculty(n-1);
+			}
+			else {
+				return 1;
+			}
+		};
 	}]);
